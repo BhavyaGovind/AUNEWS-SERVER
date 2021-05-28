785380c90b97dd72bfe731d2905a0f9036087998
Bhavya Govind <bhavyasanthu@Bhavyas-MacBook-Pro.local>
2021-05-28 14:05:45 +1000

    cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000', 'http://localhost:3001',
-    "https://bnpm-client.herokuapp.com/"##this needs an explicit list of your URLs on heroku
+    "https://bnpm-client.herokuapp.com"##this needs an explicit list of your URLs on heroku
     resource '*',
     :headers => :any,
     :methods => %i( get post put patch delete options head )
